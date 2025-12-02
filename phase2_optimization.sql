33802cc5fc82c7373b6baff4bf65196fdbd7c6e1
hallebryant <hallebryant@gmail.com>
2025-12-01 20:08:34 -0500

    Update phase2_optimization.sql

--- phase2_optimization.sql
+++ phase2_optimization.sql
@@ -5,7 +5,6 @@ CREATE INDEX IF NOT EXISTS idx_audio_track_id ON "Audio"(track_id);
 CREATE INDEX IF NOT EXISTS idx_tracks_date_recorded ON "Tracks"(track_date_recorded);
 CREATE INDEX IF NOT EXISTS idx_social_hotttnesss ON "Social"(song_hotttnesss);
 CREATE INDEX IF NOT EXISTS idx_artistlabels_label_id ON "ArtistLabels"(label_id);
-CREATE INDEX IF NOT EXISTS idx_trackgenres_genre_id ON "TrackGenres"(genre_id);
 
 -- Update stats so the planner sees the new indexes
 ANALYZE;
